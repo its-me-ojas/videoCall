217c1229907e73fdfe4b2de93db97d1072605e81
its-me-ojas <crestfallenwizard@gmail.com>
2024-04-08 00:30:18 +0530

    Added User Service

---
+++ src/main/java/com/crestfallen/videoCall/user/UserService.java
@@ -0,0 +1,42 @@
+package com.crestfallen.videoCall.user;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
+@Service
+public class UserService {
+    private static final List<User> USERS_LIST= new ArrayList<>();
+
+    public void register(User user) {
+        user.setStatus("online");
+        USERS_LIST.add(user);
+    }
+
+    public User login(User user){
+        var userIndex= IntStream.range(0,USERS_LIST.size())
+                .filter(i-> USERS_LIST.get(i).getEmail().equals(user.getEmail()))
+                .findAny()
+                .orElseThrow(() -> new RuntimeException("User not found"));
+        var cUser = USERS_LIST.get(userIndex);
+        if(!cUser.getPassword().equals(user.getPassword())){
+            throw new RuntimeException("Invalid password");
+        }
+        cUser.setStatus("online");
+        return cUser;
+    }
+
+    public void logout(User user){
+        var userIndex= IntStream.range(0,USERS_LIST.size())
+                .filter(i-> USERS_LIST.get(i).getEmail().equals(user.getEmail()))
+                .findAny()
+                .orElseThrow(() -> new RuntimeException("User not found"));
+        USERS_LIST.get(userIndex).setStatus("offline");
+    }
+
+    public List<User> findAll(){
+        return USERS_LIST;
+    }
+}
---
+++ src/test/java/com/crestfallen/videoCall/VideoCallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crestfallen.videoCall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoCallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
