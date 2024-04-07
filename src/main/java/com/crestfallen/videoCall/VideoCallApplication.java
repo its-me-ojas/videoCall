bb0e2f6c8ed3556ffc688716ef259cebd195ea92
its-me-ojas <crestfallenwizard@gmail.com>
2024-04-08 00:39:42 +0530

    Added User Controller

--- src/main/java/com/crestfallen/videoCall/VideoCallApplication.java
+++ src/main/java/com/crestfallen/videoCall/VideoCallApplication.java
@@ -1,13 +1,39 @@
 package com.crestfallen.videoCall;
 
+import com.crestfallen.videoCall.user.User;
+import com.crestfallen.videoCall.user.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class VideoCallApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(VideoCallApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(VideoCallApplication.class, args);
+    }
 
+    @Bean
+    public CommandLineRunner commandLineRunner(
+            UserService userService
+    ) {
+        return args -> {
+            userService.register(User.builder()
+                    .username("user1")
+                    .email("user1@mail.com")
+                    .password("password")
+                    .build());
+            userService.register(User.builder()
+                    .username("user2")
+                    .email("user2@mail.com")
+                    .password("password")
+                    .build());
+            userService.register(User.builder()
+                    .username("user3")
+                    .email("user3@mail.com")
+                    .password("password")
+                    .build());
+        };
+    }
 }
--- src/main/java/com/crestfallen/videoCall/user/UserService.java
+++ src/main/java/com/crestfallen/videoCall/user/UserService.java
@@ -28,9 +28,9 @@ public class UserService {
         return cUser;
     }
 
-    public void logout(User user){
+    public void logout(String email){
         var userIndex= IntStream.range(0,USERS_LIST.size())
-                .filter(i-> USERS_LIST.get(i).getEmail().equals(user.getEmail()))
+                .filter(i-> USERS_LIST.get(i).getEmail().equals(email))
                 .findAny()
                 .orElseThrow(() -> new RuntimeException("User not found"));
         USERS_LIST.get(userIndex).setStatus("offline");
