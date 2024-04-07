3928249a18c92f267bb03abfade0f0a8d41bd91a
its-me-ojas <crestfallenwizard@gmail.com>
2024-04-08 01:15:35 +0530

    Added logic for checking the user at login page

---
+++ src/main/resources/static/js/login.js
@@ -0,0 +1,33 @@
+const loginForm = document.getElementById("loginForm")
+
+function handleLogin(event) {
+    event.preventDefault()
+    // get user input
+    const email = document.getElementById("email").value
+    const password = document.getElementById("password").value
+
+    const user = {
+        email: email,
+        password: password
+    }
+
+    fetch('http://localhost:8080/api/users/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(user)
+    }).then(res => {
+        if (!res.ok) {
+            alert("Invalid email or password")
+        }
+        return res.json();
+    }).then(res => {
+        localStorage.setItem('connectedUser', JSON.stringify(res))
+        window.location.href = 'index.html'
+    }).catch(err => {
+        console.error('POST request error: ', err)
+    })
+}
+
+loginForm.addEventListener("submit", handleLogin)
